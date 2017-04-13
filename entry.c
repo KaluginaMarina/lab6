a41637b352ea4ac377064818b8d78773d17c56f9
KaluginaMarina <21mk07@mail.ru>
2017-04-13 16:19:15 +0300

    Add files via upload

---
+++ entry.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include "my_string.h"
+
+int entry(char *str)
+{
+    if (str == NULL)
+    {
+        return 1;
+    }
+    printf("¬ведите строку:\n");
+    char s = ' ';
+    int n = 0;
+    while (s != '\n')
+    {
+        scanf("%c", &s);
+        *(str + n) = s;
+        ++n;
+        if (n > N)
+        {
+            return 1;
+        }
+    }
+    --n;
+    *(str + n) = '\0';
+    return 0;
+}
